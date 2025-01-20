8edee775ae518940ce949d9719c60e1639760e89
Lam Weng Sart <86751654+Lucuslam0826@users.noreply.github.com>
2025-01-20 16:57:30 +0800

    Add files via upload

--- web.php
+++ web.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-
-// Task routes
-Route::prefix('tasks')->group(function () {
-    Route::get('/', [TaskController::class, 'getTasks']);
-    Route::post('/create', [TaskController::class, 'addTask']);
-    Route::put('/update/{id}', [TaskController::class, 'updateTask']);
-    Route::delete('/remove/{id}', [TaskController::class, 'deleteTask']);
-});
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/newtask', function () {
+    return view('newtask');
 });
